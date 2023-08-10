df89d87db3ea392b1c449dae1671e8ecf411719e
Davi Ribeiro <daviribeiro@MacBook-Air-de-Davi.local>
2023-08-10 18:31:28 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+
+        }
+    })
+
+})
